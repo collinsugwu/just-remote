f4aea136b2fbbb8350402f312298ce0d268f69ed
collins <ugwucollins64@gmail.com>
2019-11-24 16:36:00 +0100

    scrap class moved to libs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class JobsController < ApplicationController
-  require_relative 'scrap'
 
   before_action :set_job, only: %i[show update destroy]
   def index
@@ -20,27 +19,7 @@ class JobsController < ApplicationController
     @job = Job.new(job_param)
     @job.save
   end
-
-  def create_scrap_job
-    scrap = Scrap.new
-    @data = scrap.job_links
-    @data.each do |data|
-      ActiveRecord::Base.transaction do
-        job = Job.new
-        job.title = data[:title]
-        job.save
-
-        job_detail = JobDetail.new
-        job_detail.job_id = job.id
-        job_detail.description = data[:description]
-        job_detail.location = data[:location]
-        job_detail.recruiter = data[:recruiter]
-        job_detail.stack = data[:stack]
-        job_detail.save
-      end
-    end
-  end
-
+  
   def update; end
 
   def destroy; end
